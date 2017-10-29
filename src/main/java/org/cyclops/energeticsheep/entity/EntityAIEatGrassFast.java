54e03f5b5f1f6ce62f85bff9d0135fa96b55441b
Ruben Taelman <rubensworks@gmail.com>
2017-10-29 15:02:07 +0900

    Don't eat grass when sheep still has wool

--- src/main/java/org/cyclops/energeticsheep/entity/EntityAIEatGrassFast.java
+++ src/main/java/org/cyclops/energeticsheep/entity/EntityAIEatGrassFast.java
@@ -5,7 +5,6 @@ import com.google.common.base.Predicates;
 import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.state.pattern.BlockStateMatcher;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAIEatGrass;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -19,16 +18,17 @@ public class EntityAIEatGrassFast extends EntityAIEatGrass {
     private static final Predicate<IBlockState> IS_TALL_GRASS = BlockStateMatcher.forBlock(Blocks.TALLGRASS)
             .where(BlockTallGrass.TYPE, Predicates.equalTo(BlockTallGrass.EnumType.GRASS));
 
-    private final EntityLiving grassEaterEntity;
+    private final EntityEnergeticSheep grassEaterEntity;
 
-    public EntityAIEatGrassFast(EntityLiving grassEaterEntityIn) {
+    public EntityAIEatGrassFast(EntityEnergeticSheep grassEaterEntityIn) {
         super(grassEaterEntityIn);
         this.grassEaterEntity = grassEaterEntityIn;
     }
 
     @Override
     public boolean shouldExecute() {
-        if (this.grassEaterEntity.getRNG().nextInt(this.grassEaterEntity.isChild() ? 10 : 200) != 0) {
+        if (!this.grassEaterEntity.getSheared()
+                || this.grassEaterEntity.getRNG().nextInt(this.grassEaterEntity.isChild() ? 10 : 200) != 0) {
             return false;
         } else {
             BlockPos blockpos = new BlockPos(this.grassEaterEntity.posX, this.grassEaterEntity.posY, this.grassEaterEntity.posZ);
